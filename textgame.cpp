f9a1a316cb3798f1fffb622a5778f487e553ca11
superstaroof <62721803+superstaroof@users.noreply.github.com>
2020-06-29 23:46:20 +0200

    tedtgame.cpp
    the game

---
+++ textgame.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+int x;
+cout << "Right is Win Left is Lose, Select Using R or W";
+cin >> x;
+if x = R(){cout << "win"};
+if x = W(){cout << "lose"};
+return 0;
+}
